f87a7d5a3b040260566beb6b3890bb09befcd844
Oryan Abergel <86202122+Oryanab@users.noreply.github.com>
2024-07-28 00:30:17 +0200

    update init structure

---
+++ types/index.ts
@@ -0,0 +1,6 @@
+export interface AuthProps {
+  authState?: { token: string | null; authenticated: boolean | null };
+  onRegister?: (email: string, password: string) => Promise<any>;
+  onLogin?: (email: string, password: string) => Promise<any>;
+  onLogout?: () => Promise<any>;
+}
